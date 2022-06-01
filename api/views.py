fe9a4497c76f92b8d7bd0d2b667921b8713a704c
Jean-Pierre Ciotta <jpciotta@gmail.com>
2022-06-01 16:25:15 -0700

    vid 3

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from .serializers import RoomSerializer
 from .models import Room
 
 # Create your views here.
-
-class RoomView(generics.ListAPIView):
+#list/Create APIVIEW
+class RoomView(generics.CreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
