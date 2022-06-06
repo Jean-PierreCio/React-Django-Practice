bf65f500ff66348e4728460d8723ab67e2f32c87
Jean-Pierre Ciotta <jpciotta@gmail.com>
2022-06-05 17:11:45 -0700

    video 4

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route exact path="/"  elements={<p>This is the home page</p>} />
+          <Route exact path="/join" element={<RoomJoinPage />} />
+          <Route exact path="/create" element={<CreateRoomPage />} />
+        </Routes>
+      </Router>
+    );
+  }
+}
